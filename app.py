9969ed2ca9aa822702dd5a42944aafbd69cbdafd
David <daviddelsol1998@gmail.com>
2021-10-27 19:17:19 -0600

    added needed modules

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # import dependencies
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
