4d34fac69b2b9f68c49346d15ec4a06c2e83e68e
David <daviddelsol1998@gmail.com>
2021-10-27 19:16:56 -0600

    updated integration

--- app.py
+++ app.py
@@ -1,6 +1,71 @@
-from flask import Flask, render_template, request, redirect, url_for
+# import dependencies
+from flask import Flask, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.fields.simple import SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 
+# setup flask app
 app = Flask(__name__)
+# setup sqlalchemy db for the app
+db = SQLAlchemy(app)
+# setup sqlalchemy db file
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+# setup secret key for app
+app.config['SECRET_KEY'] = 'DevelopeandoPlanetas'
+# setup encryption for passwords
+bcrypt = Bcrypt(app)
+
+# create db model for users_table
+
+
+class UserTable(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    # the length of the password is longer due to hashing
+    password = db.Column(db.String(80), nullable=False)
+
+# setup flask forms
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=40)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField("Create account")
+
+    # validate if user exists
+    def user_validation(self, username):
+        existing_username = UserTable.query.filter_by(
+            username=username.data).first()
+        if existing_username:
+            raise ValidationError(
+                "Username already in use, please pick a different one.")
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=40)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField("Login")
+
+
+# setup login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return UserTable.query.get(int(user_id))
 
 
 if __name__ == '__main__':
